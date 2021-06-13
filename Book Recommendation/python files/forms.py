9d5fb0025a1d5be3fcc686210c7dbe065739f2d8
mikhaelamartin <36165469+mikhaelamartin@users.noreply.github.com>
2021-06-13 16:45:09 -0600

    Delete Book Recommendation directory

--- Book Recommendation/python files/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, AnyOf
-
-class SearchForm(FlaskForm):
-	search = StringField('Search Book',validators=[DataRequired(),AnyOf])
-	submit = SubmitField('Enter')
